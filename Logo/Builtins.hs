c389cf46222f8d2953ac3e9dfc5aa1ad7591eab4
Deepak Jois <deepak.jois@gmail.com>
2012-02-04 19:39:11 -0800

    Define working fd function

--- Logo/Builtins.hs
+++ Logo/Builtins.hs
@@ -2,11 +2,21 @@ module Logo.Builtins where
 
 import Logo.Types
 import Logo.Turtle
+import Text.Parsec.Prim
+import Control.Applicative ((<$>))
 
 import qualified Data.Map as M
 
 fd :: [LogoToken] -> LogoEvaluator LogoToken
-fd ((NumLiteral d):[]) = undefined  -- (LogoContext t f) = unLogoContext (forward d t) f
+fd ((NumLiteral d):[]) = do
+  updateTurtleState (forward d)
+  return $ StrLiteral ""
+
+updateTurtleState :: (Turtle -> Turtle) -> LogoEvaluator ()
+updateTurtleState f = do
+  s <- getState
+  let t = turtle s
+  putState $ s { turtle = f t }
 
 builtins = M.fromList
   [("fd", LogoFunctionDef 1 fd)]
