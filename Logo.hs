917a971b075f239f9ebd5b4d993aad2a5a2fea4d
Deepak Jois <deepak.jois@gmail.com>
2012-02-12 12:43:39 -0800

    Accept files as input. Fixes #16

--- Logo.hs
+++ Logo.hs
@@ -1,43 +1,64 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
+
+import Control.Applicative ((<$>))
 
 import Diagrams.Prelude
 import Diagrams.Backend.Cairo.CmdLine
-import Logo.Turtle
+
 import Logo.Types
 import Logo.TokenParser
 import Logo.Builtins
 import Logo.Evaluator
+
+import System.Environment (getProgName, withArgs)
+
+import System.Console.CmdArgs.Implicit
+
 import qualified Data.Map as M
 
 
+data LogoOpts = LogoOpts
+  { output :: String -- output file to write to
+  , src    :: Maybe String -- source file to read from
+  } deriving (Show, Data, Typeable)
+
+logoOpts :: String -> LogoOpts
+logoOpts prog = LogoOpts
+  { output =  "logo.png"
+           &= typFile
+           &= help "Output image file (default=logo.png)"
+  , src = def
+        &= typFile
+        &= args
+  }
+  &= summary "hs-logo Logo Interpreter"
+  &= program prog
+
 main :: IO ()
-main = defaultMain (stroke logo # lw 0.4)
- where
-   fromRight (Right x) = x
-   tokens = fromRight $ tokenize snowflake
-   t = Turtle True 0 (Path [(P (0,0), Trail [] False)])
-   ctx = LogoContext t builtins (M.empty)
-   res = evaluateWithContext tokens ctx
-   getLogoPath t@(Turtle _ _ p) =  p
-   logo = case res of
-            (_,LogoContext t _ _) -> getLogoPath t
+main = do
+  prog <- getProgName
+  opts <- cmdArgs (logoOpts prog)
+  case src opts of
+    Nothing -> error "Source file not specified"
+    Just s  -> renderLogo s (output opts)
 
+renderLogo :: String -> String -> IO ()
+renderLogo s o = do
+  tokens <- readSource s
+  let path = evaluateSourceTokens tokens
+  withArgs ["-o", o] $ defaultMain (stroke path # lw 0.4)
 
-snowflake = "\
-\to side :size :level \
-\ifelse :level = 0 \
-\[ \
-\fd :size \
-\] \
-\[ \
-\side :size / 3 :level - 1 \
-\lt 60 \
-\side :size / 3 :level - 1 \
-\rt 120 \
-\side :size / 3 :level - 1 \
-\lt 60 \
-\side :size / 3 :level - 1 \
-\] \
-\end \
-\lt 30 \
-\repeat 3 [side 250 4 rt 120]"
+readSource :: FilePath -> IO [LogoToken]
+readSource f = do
+  tokens <- tokenize <$> readFile f
+  case tokens of
+    Left x -> error $ show x
+    Right t -> return t
+
+evaluateSourceTokens :: [LogoToken] -> Path R2
+evaluateSourceTokens tokens = do
+  let t                   = Turtle True 0 (Path [(P (0,0), Trail [] False)])
+      initialContext      = LogoContext t builtins M.empty
+  case evaluateWithContext tokens initialContext of
+    (_,LogoContext (Turtle _ _ p) _ _) -> p
