e3040318ff542f42cb10cd5670088eb554a5faae
Deepak Jois <deepak.jois@gmail.com>
2012-02-13 18:26:10 -0800

    removing unused imports

--- Logo/TokenParser.hs
+++ Logo/TokenParser.hs
@@ -3,14 +3,13 @@ module Logo.TokenParser where
 import Logo.Types
 
 import Control.Applicative ((<|>), (<$>), many)
-import Control.Monad (ap)
 
 import Text.ParserCombinators.Parsec (
   char, letter, alphaNum, string, space,
   parse, many1, skipMany, skipMany1, sepEndBy1, noneOf, try,
   ParseError, Parser)
 
-import Text.ParserCombinators.Parsec.Number (floating2, int, natFloat, sign)
+import Text.ParserCombinators.Parsec.Number (natFloat, sign)
 
 tokenize :: String -> Either ParseError [LogoToken]
 tokenize = parse logo "(unknown)"
