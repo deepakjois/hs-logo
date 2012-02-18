9fcd6b27b1a262518b93c6f421fe44abf2f11150
Deepak Jois <deepak.jois@gmail.com>
2012-02-18 13:34:43 -0800

    Initial commit of hs-logo site

--- site/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ghc -Wall -fno-warn-orphans -fno-warn-unused-do-bind  --make Site.hs
-
-
-
--- site/examples/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for i in `ls sources/*.logo`
-do
-  out="images/`basename $i .logo`.png"
-  echo "Running $i"
-  ../dist/build/hs-logo/hs-logo $i -o $out
-done
