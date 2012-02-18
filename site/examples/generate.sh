66fef080fe830b1dd1f4836b05db395dccf82c2e
Deepak Jois <deepak.jois@gmail.com>
2012-02-18 15:30:21 -0800

    More examples

--- site/examples/generate.sh
+++ site/examples/generate.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
+
+set -e
+
+runlogo() {
+  out="images/`basename $1 .logo`.png"
+  echo "Running $i"
+  ../../dist/build/hs-logo/hs-logo $1 -o $out
+}
+
+# Run just once if a filename is passed in
+if [ $# = 1 ]
+then
+  runlogo $1
+  exit
+fi
+
 for i in `ls sources/*.logo`
 do
   out="images/`basename $i .logo`.png"
   echo "Running $i"
-  ../dist/build/hs-logo/hs-logo $i -o $out
+  runlogo $i
 done
