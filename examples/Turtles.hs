3f9c8382b6fc0d88de4f1f5ead7e570d6977eb77
Deepak Jois <deepak.jois@gmail.com>
2012-04-07 12:31:14 -0700

    Update turtle examples

--- examples/Turtles.hs
+++ examples/Turtles.hs
@@ -1,17 +1,24 @@
 -- | Examples of diagrams using the Turtle data type
-
 import Diagrams.Prelude
-import Diagrams.TwoD.Path.Turtle.Internal (startTurtleState, forward, right, getTurtleDiagram)
+import Diagrams.TwoD.Path.Turtle.Internal
+import Diagrams.Backend.SVG
 import Diagrams.Backend.SVG.CmdLine (defaultMain)
 
+
+
+instance Show Turtle where
+  show t@(Turtle a b c _ _ _) = show (a,b,c)
+
+squareTurtle :: Diagram SVG R2
 squareTurtle = getTurtleDiagram $
  startTurtleState #
-  forward x # right turn #
-  forward x # right turn #
-  forward x # right turn #
+  forward x # right turn # setPenWidth 2.0 # setPenColour blue  #
+  forward x # right turn # setPenWidth 3.0 # setPenColour red   #
+  forward x # right turn # setPenWidth 0.5 # setPenColour green #
   forward x
  where
   x = 2.0
   turn = 90
+
 
 main = defaultMain (squareTurtle # centerXY # pad 1.1)
